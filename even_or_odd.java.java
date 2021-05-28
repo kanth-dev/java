0ab73e0b30f891d2c30f33d2524ffaabcb5a0d51
kanth <kanthpubg123@gmail.com>
2021-05-28 10:05:16 +0530

    modified even_or_odd.java

---
+++ even_or_odd.java.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class EvenorOddUsingIf {
+	private static Scanner sc;
+	
+	public static void main(String[] args) {
+		int Number;
+		sc = new Scanner(System.in);		
+		System.out.println("\n Please Enter any integer Value: ");
+		Number = sc.nextInt();
+		
+		if (Number % 2 == 0) {
+			System.out.println("\n You have entered EVEN Number");
+		}
+		else {
+			System.out.println("\n You have entered ODD Number");
+		}
+	}
+}
