e6a5afaafa60f828c0cd982cef98b90434212fca
kanth <kanthpubg123@gmail.com>
2021-05-28 10:51:12 +0530

    mult.java

---
+++ mult.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+class Mul
+{
+public static void main(String[] args)
+{
+int x,y;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter first number");
+x=sc.nextInt();
+System.out.println("Enter first number");
+y=sc.nextInt();
+int z=x*y;
+System.out.println("Multiplication of two numbers is : "+z);
+}
+}
